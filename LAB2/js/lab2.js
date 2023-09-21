3d65ab87655e11acefcea907a3089565ccf326aa
Dylan <dgbarkle@iu.edu>
2023-09-20 18:56:29 -0900

    Init Lab 2. This class is suffering

---
+++ LAB2/js/lab2.js
@@ -0,0 +1,10 @@
+/*
+TITLE: LAB-2 Nines Balls
+AUTHOR: Dylan Barkley (DGB)
+CREATE DATE: 20 Sep 2023
+PURPOSE: Interactivity for a lottery simulator page
+LAST MODIFIED ON: -
+LAST MODIFIED BY: Dylan Barkley (DGB)
+MODIFICATION HISTORY:
+
+*/
