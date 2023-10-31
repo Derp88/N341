fef596fe11f5c4815077b0dd1b6147788a63d96e
Dylan <dgbarkle@iu.edu>
2023-10-31 18:51:57 -0400

    Init LAB 4

---
+++ LAB4/js/lab4.js
@@ -0,0 +1,10 @@
+/*
+TITLE: LAB-4 Nines Balls
+AUTHOR: Dylan Barkley (DGB)
+CREATE DATE: 31 Oct 2023
+PURPOSE: Interactivity for a lottery simulator page
+LAST MODIFIED ON: - 31 Oct 2023
+LAST MODIFIED BY: Dylan Barkley (DGB)
+MODIFICATION HISTORY:
+    31 Oct 2023 - 
+*/
