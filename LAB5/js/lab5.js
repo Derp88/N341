7cba8b4d08ef939c417d75bfd027aa242935f453
Dylan <dgbarkle@iu.edu>
2023-11-26 05:27:18 -0500

    LAB 5 halfway done

---
+++ LAB5/js/lab5.js
@@ -0,0 +1,12 @@
+/*
+TITLE: LAB-5 Nines Balls
+AUTHOR: Dylan Barkley (DGB)
+CREATE DATE: 25 Nov 2023
+PURPOSE: Interactivity for a lottery simulator page
+LAST MODIFIED ON: - 25 Nov 2023
+LAST MODIFIED BY: Dylan Barkley (DGB)
+MODIFICATION HISTORY:
+    
+*/
+$("#tabs").tabs();
+$("#numberInput").spinner();
