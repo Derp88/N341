97e42b899f22b9bd72429015bef7570ef673d068
Dylan <dgbarkle@iu.edu>
2023-11-28 17:46:03 -0500

    Init Final

--- LAB5/js/lab5.js
+++ LAB5/js/lab5.js
@@ -8,5 +8,25 @@ LAST MODIFIED BY: Dylan Barkley (DGB)
 MODIFICATION HISTORY:
     
 */
-$("#tabs").tabs();
-$("#numberInput").spinner();
+$("#tabs").tabs(); //REQ 4: Use tab widget
+$("#numberInput").spinner(); //REQ 5: use spinner widget
+
+$("#mainForm").on("submit", function(e){ //REQ 9: Echo output
+    e.preventDefault();
+    var vals = $(this).serializeArray();
+    $("#ageSubmit").text("Over 18: " + vals[1].value); //REQ 10: Get data using .value
+    $("#emailSubmit").text("Email: " + vals[2].value);
+    $("#ticketSubmit").text("Ticket Number: " + vals[0].value);
+});
+
+$("#numberInput").focus(); //REQ 12: Use form method
+
+$("#numberInput").on("blur", function(){ //REQ 13: Use .on() with form event
+    var valid = /^[0-9]+$/.test(this.value); //REQ 11: Regex validation
+    if(!valid){
+        $("#numFeedback").text("Feedback: Current input is INVALID");
+    }else{
+        $("#numFeedback").text("Feedback: Current input is valid");
+    }
+});
+
